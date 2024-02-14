7a6b2ed20a171606682d8bd670351eac837e9c4b
Daniel Salameh <danielsalameh04@gmail.com>
2024-02-14 14:49:44 -0800

    Commented on header

--- header.h
+++ header.h
@@ -1,3 +1,13 @@
+/************************************************************************
+ * CourseGrader
+ * Project by Daniel Salameh
+ * 
+ * This program keeps track of multiple courses and multiple students. Students are able to attend multiple courses at the same time.
+ * You are able to grade students on assignments throught he course class as well as track the gradebook. Students have their own profiles
+ * that can be accessed and display the student's grades for the classes they are in.
+ * 
+*************************************************************************/
+
 #ifndef HEADER_H_
 #define HEADER_H_
 
@@ -15,24 +25,28 @@ class Gradebook;
 
 class Course {
     public:
+        // Constructors
         Course();
         Course(string instruct);
         Course(string instruct, string n);
         Course(string instruct, string n, string desc);
+        // Accessors / Print
         void const printStudents();
         void const printAssignments();
         void const printFinalGrades();
         void const printStudentsGrades(Student* student);
         void const printDetails();
+
         void addStudent(Student* addedStudent);
         void addAssignment(Assignment* addedAssignment);
         void gradeStudent(Student* student, Assignment* assignment, double score);
+
         void print();
     private:
         string instructor;
         string name;
         string description;
-        unordered_map<Student*,Gradebook*> students; //acts as roster and gradebook holder
+        unordered_map<Student*,Gradebook*> students; //Acts as roster and gradebook holder
         vector<Assignment*> assignments;
         bool isStudentInCourse(Student* checkStudent);
         bool isAssignmentInCourse(Assignment* checkAssignment);
@@ -40,14 +54,18 @@ class Course {
 
 class Student {
     public:
+        // Constructors
         Student();
         Student(string n);
         Student(string n, int num);
+        // Accessors / Print
         string const getName();
         void printDetails();
         void printGrades();
+
         void addGradebook(string const courseName, Gradebook* const gbook);
         bool isAssignmentInStudent(string const courseName, Assignment* const assignment);
+
         void print();
     private:
         string name;
@@ -56,12 +74,15 @@ class Student {
 };
 class Assignment {
     public:
+        // Constructors
         Assignment();
         Assignment(string n);
         Assignment(string n, string desc);
         Assignment(string n, string desc, double total);
+        // Accessors / Print
         double const getTotalPoints();
         string const getName();
+
         void print();
     private:
         string name;
@@ -71,14 +92,18 @@ class Assignment {
 
 class Gradebook {
     public:
+        // Constructors
         Gradebook();
+
         void addEntry(Assignment* key, double score);
         char getLetterGrade();
         bool isAssignmentInGradebook(Assignment* const assignment);
         void setScore(Assignment* gradingAssignment, double score);
+
         void const printScores();
     private:
         vector<pair<Assignment*, double>> grades;
+        // Helper function
         double getPercent();
 };
 
